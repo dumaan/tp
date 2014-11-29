806cd60bd98c4147f12b70faa8e866a850f062f5
Farzad <farzad.fbo@gmail.com>
2014-11-30 00:16:23 +0330

    Signed-off-by: Farzad <farzad.fbo@gmail.com>

---
+++ src/main/java/com/farzad/core/tp/App.java
@@ -0,0 +1,13 @@
+package com.farzad.core.tp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
